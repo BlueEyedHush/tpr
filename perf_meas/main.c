67f5bf4c36f2b47df2b9d8594afb2b8de1a76c86
knawara <blueeyedhush@gmail.com>
2017-03-13 23:53:56 +0100

    added custom datatype registration; changed buffer size to take into account some additional metadata

--- perf_meas/main.c
+++ perf_meas/main.c
@@ -2,21 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef unsigned char byte;
 
+#define MSGS_TOTAL_SIZE 100000001 // number of bytes transfered
+#define MSGS_SIZE 100000000 // number of bytes of meaningful data
 #define MSG_CONF(SIZE) {SIZE, 100000000/SIZE} // total size always 10^8
 #define NUM_MSG_CONFS 12
-int msg_confs[][2] = {MSG_CONF(1),
-                      MSG_CONF(4),
-                      MSG_CONF(16),
-                      MSG_CONF(64),
-                      MSG_CONF(128),
-                      MSG_CONF(512),
-                      MSG_CONF(2048),
-                      MSG_CONF(4096),
-                      MSG_CONF(8192),
-                      MSG_CONF(16384),
-                      MSG_CONF(32768),
-                      MSG_CONF(65536)};
+const int msg_confs[][2] = {MSG_CONF(1),
+                            MSG_CONF(4),
+                            MSG_CONF(16),
+                            MSG_CONF(64),
+                            MSG_CONF(128),
+                            MSG_CONF(512),
+                            MSG_CONF(2048),
+                            MSG_CONF(4096),
+                            MSG_CONF(8192),
+                            MSG_CONF(16384),
+                            MSG_CONF(32768),
+                            MSG_CONF(65536)};
+
+byte buffer[MSGS_TOTAL_SIZE] = {0}; // one is reserved to store id into msg_confs array to make responding easier
+
+MPI_Datatype datatypes[NUM_MSG_CONFS] = {0};
+
+void register_datatypes() {
+    for(int i = 0; i < NUM_MSG_CONFS; i++) {
+        int size = msg_confs[i][0];
+        MPI_Type_contiguous(size, MPI_BYTE, &datatypes[i]);
+        MPI_Type_commit(&datatypes[i]);
+    }
+}
 
 void pong_main() {
     int number = -1;
@@ -42,6 +57,8 @@ int main(int argc, char **argv) {
         fprintf(stderr, "World size must be greater than 1 for %s\n", argv[0]);
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
+
+    register_datatypes();
 
     if (world_rank == 0) {
         pong_main();
