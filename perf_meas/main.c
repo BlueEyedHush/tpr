ccb1c5764d45790e7ce7faed196b65d77a5d3009
knawara <blueeyedhush@gmail.com>
2017-03-13 23:10:07 +0100

    adding required message configurations

--- perf_meas/main.c
+++ perf_meas/main.c
@@ -2,7 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv) {
+
+#define MSG_CONF(SIZE) {SIZE, 100000000/SIZE} // total size always 10^8
+#define NUM_MSG_CONFS 12
+int msg_confs[][2] = {MSG_CONF(1),
+                               MSG_CONF(4),
+                               MSG_CONF(16),
+                               MSG_CONF(64),
+                               MSG_CONF(128),
+                               MSG_CONF(512),
+                               MSG_CONF(2048),
+                               MSG_CONF(4096),
+                               MSG_CONF(8192),
+                               MSG_CONF(16384),
+                               MSG_CONF(32768),
+                               MSG_CONF(65536)};
+
+int main(int argc, char **argv) {
     MPI_Init(NULL, NULL);
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
