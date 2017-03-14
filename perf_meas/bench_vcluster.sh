5979e4333a0a9cd6c1086dcea9b82a38f3475e89
knawara <blueeyedhush@gmail.com>
2017-03-14 02:54:51 +0100

    adding script to automate benchmarking

---
+++ perf_meas/bench_vcluster.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+RES_DIR="$DIR"/results
+mkdir -p "$RES_DIR"
+
+N1="$RES_DIR"/n1
+N2="$RES_DIR"/n2
+
+for i in "N1" "N2"; do
+    echo -e "\nNew run\n" >> "$i"
+done
+
+"$DIR"/run_locally.sh >> "$N1"
+"$DIR"/run_vcluster.sh >> "$N2"
