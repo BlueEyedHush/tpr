3bf17bf3c7e9580a65a71f03bf0f360cb9b3c3a2
knawara <blueeyedhush@gmail.com>
2017-03-20 21:15:01 +0100

    common build.sh file

--- broadcast/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$(dirname "${BASH_SOURCE[0]}")"
-
-mkdir -p "$DIR"/out/
-mpicc main.c -cc=gcc -o "$DIR"/out/broadcast_custom -D CUSTOM -std=c99
-#mpicc main.c -cc=gcc -o "$DIR"/out/broadcast -std=c99
---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    echo "Need to specify project as first argument"
+    exit 1
+else
+    PROJ="$1"
+fi
+
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+PROJ_DIR="$DIR/$PROJ/"
+OUT_DIR="$DIR/out/$PROJ/"
+
+mkdir -p "$OUT_DIR"
+
+if [ "$PROJ" == "perf_meas" ]; then
+    mpicc "$PROJ_DIR"main.c -cc=gcc -o "$OUT_DIR"sync -D SYNC -std=c99
+    mpicc "$PROJ_DIR"main.c -cc=gcc -o "$OUT_DIR"buff -D BUFFERED -std=c99
+    mpicc "$PROJ_DIR"main.c -cc=gcc -o "$OUT_DIR"sync_lat -D SYNC -D LATENCY -std=c99
+    mpicc "$PROJ_DIR"main.c -cc=gcc -o "$OUT_DIR"buff_lat -D BUFFERED -D LATENCY -std=c99
+else
+    mpicc "$PROJ_DIR"main.c -cc=gcc -o "$OUT_DIR"exec -std=c99
+fi
