dd29ddc11b441661d6165d8d6d3c796141b401a1
knawara <blueeyedhush@gmail.com>
2017-03-20 22:03:12 +0100

    common run_locally.sh script

--- broadcast/run_locally.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$(dirname "${BASH_SOURCE[0]}")"
-
-mpiexec -np 5 "$DIR"/out/broadcast_custom
-mpiexec -np 5 "$DIR"/out/broadcast
---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    echo "Need to specify project as first argument"
+    exit 1
+else
+    PROJ="$1"
+fi
+
+REL_DIR="$(dirname "${BASH_SOURCE[0]}")"
+DIR="$(readlink -e $REL_DIR)"
+PROJ_DIR="$DIR/$PROJ/"
+OUT_DIR="$DIR/out/$PROJ/"
+
+if [ "$2" == "local" ]; then
+    VCLUSTER_OPTS=""
+elif [ "$2" == "vcluster" ]; then
+    VCLUSTER_OPTS="-machinefile vcluster_hosts"
+else
+    echo "Need to specify target (local|vcluster) as second argument"
+    exit 1
+fi
+
+if [ "$PROJ" == "reduce" ]; then
+    PROC_OPTS="-np 4"
+    ARGS="" # HERE GO ADDITIONAL ARGUMENTS FOR REDUCE!
+elif [ "$PROJ" == "broadcast" ]; then
+    PROC_OPTS="-np 5"
+else
+    echo "Project must be one of reduce|broadcast"
+    exit 1
+fi
+
+CMD="mpiexec $PROC_OPTS $VCLUSTER_OPTS ${OUT_DIR}exec $ARGS"
+echo "$CMD"
+
+$CMD
