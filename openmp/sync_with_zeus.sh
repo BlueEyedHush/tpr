213b3f767ef852301e6774d433a5ea22b471d9b4
knawara <blueeyedhush@gmail.com>
2017-04-19 11:47:58 +0200

    [OpenMP] untracking some files that were ignored

--- openmp/sync_with_zeus.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-REL_DIR="$(dirname "${BASH_SOURCE[0]}")"
-DIR="$(readlink -e $REL_DIR)"
-
-sshpass -p 97e03130B234 rsync -avzr "$DIR"/. plgblueeyedhush@zeus.cyfronet.pl:/people/plgblueeyedhush/openmp
